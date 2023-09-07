c02891ba5b3c555276a61538150d7fd929b71b32
Gabriela Queiroz <gabiqss1@gmail.com>
2023-09-07 19:07:38 -0300

    Create index.js

---
+++ aula3/index.js
@@ -0,0 +1,12 @@
+let nome;
+nome = 'qualquer valor';
+console.log(nome)
+
+let nomeCompleto = 'gabi'
+console.log(nomeCompleto)
+
+const numero1 = 15;
+const numero = 2;
+console.log(typeof(numero))
+const multiplicacao = numero1 * numero
+console.log(multiplicacao)
