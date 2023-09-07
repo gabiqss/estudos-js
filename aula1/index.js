73728a849ede3045f22c15c628fbfb38042aca59
Gabriela Queiroz <gabiqss1@gmail.com>
2023-09-07 18:39:11 -0300

    07/09

---
+++ aula1/index.js
@@ -0,0 +1,5 @@
+console.log('gabi');
+console.log("gabi");
+console.log(`gabi`);
+
+console.log(35, 15.85, 'gabi');
---
+++ aula2/index.js
@@ -0,0 +1,4 @@
+const nome = 'gabi';
+const hora = 18;
+
+console.log('meu nome é', nome, '.', 'estou aprendendo javascript às', hora);
